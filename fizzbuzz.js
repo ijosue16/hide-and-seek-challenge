87d6e965d6ae6581323f2301ed5d9c0317cc2d42
ijosue16 <111968510+ijosue16@users.noreply.github.com>
2022-11-26 19:37:36 +0200

    Update fizzbuzz.js

--- fizzbuzz.js
+++ fizzbuzz.js
@@ -1,14 +1,14 @@
 module.exports = function () {
 const result =[];
 for(let i=1;i<=100;i++){
-  if(i%5===0 && i%3===0){
-    result.push("FizzBuzz");
-  }
-  else if(i%3===0){
+  if(i%3===0){
     result.push("Fizz");
   }
   else if(i%5===0){
     result.push("Buzz");
+  }
+  else if(i%5===0 && i%3===0){
+    result.push("FizzBuzz");
   }
   else{
     result.push(i);
